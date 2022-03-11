a7c751be1829f546a92cd69587bbeca2361043c6
Oscar Silva <oscarsilva2356@gmail.com>
2022-03-10 21:53:48 -0800

    Added some update to the SavedBooks.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -39,9 +39,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
-
-      if (!response.ok) {
+      const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${searchInput}`
+      );
+      
+        if (!response.ok) {
         throw new Error("something went wrong!");
       }
 
